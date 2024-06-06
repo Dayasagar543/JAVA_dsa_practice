a019aff8783233b8f0071594f654f46bd4676094
Dayasagar543 <dayasagar.543@gmail.com>
2024-06-06 10:13:15 +0530

    adding the files char.java ad try_this folder with Try_this.java file

---
+++ input/Char.java
@@ -0,0 +1,20 @@
+package JAVA_dsa_practice.input;
+
+// import java.util.Scanner;
+
+public class Char {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        char ch = '!';
+        int val0=ch;
+        int val1 = 'a';
+        int val2 = 'A';
+        System.out.println(val0);
+        System.out.println(val1);
+        System.out.println(val2);
+        // type casting
+        // int val =(int)ch;
+
+    }
+
+}
