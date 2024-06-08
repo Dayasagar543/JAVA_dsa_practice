4de26c3284103e2daa9d0ffd6d955e7a203519a8
Dayasagar543 <dayasagar.543@gmail.com>
2024-06-08 12:24:51 +0530

    addes straight line file

--- conditionals/Ages.java
+++ conditionals/Ages.java
@@ -11,14 +11,29 @@ public class Ages {
         int person2 = sc.nextInt();
         System.out.println("please input the age of third person");
         int person3 = sc.nextInt();
-        if (person1 < person2 && person1 < person3) {
-            System.out.println("person1 is younger " + person1);
-        } else if (person2 < person3) {
-            System.out.println("person2 is younger " + person2);
+        // if (person1 < person2 && person1 < person3) {
+        // System.out.println("person1 is younger " + person1);
+        // } else if (person2 < person3) {
+        // System.out.println("person2 is younger " + person2);
+        // } else {
+        // System.out.println("person3 is younger " + person3);
+        // }
+
+        if (person1 < person2) {
+            if (person1 < person3) {
+                System.out.println("person 1 is younger " + person1);
+            } else {
+                System.out.println("person 3 is younger " + person3);
+            }
         } else {
-            System.out.println("person3 is younger " + person3);
+            if (person2 < person3) {
+                System.out.println("person 2 is younger " + person2);
+            } else {
+                System.out.println("person 3 is younger " + person3);
+            }
         }
         sc.close();
+
     }
 
 }
---
+++ conditionals/Straight_line.java
@@ -0,0 +1,18 @@
+package JAVA_dsa_practice.conditionals;
+
+import java.util.Scanner;
+
+public class Straight_line {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int x1=sc.nextInt();
+        int x2=sc.nextInt();
+        int x3=sc.nextInt();
+        int y1=sc.nextInt();
+        int y2=sc.nextInt();
+        int y3=sc.nextInt();
+      
+       
+    }
+    
+}
