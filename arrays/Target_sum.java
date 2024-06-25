141c98aa5b5bdf8c04e7de939c24eab6d3d38b61
Dayasagar543 <dayasagar.543@gmail.com>
2024-06-25 13:52:02 +0530

    added Target_sum.java

---
+++ arrays/Target_sum.java
@@ -0,0 +1,17 @@
+package JAVA_dsa_practice.arrays;
+
+public class Target_sum {
+    public int sum_target(int []array,int value){
+        int count =0;
+        for(int i =0;i<array.length;i++){
+            for(int j=i+1;j<array.length;j++){
+                if(array[i]+array[j]==value){
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+    
+}
