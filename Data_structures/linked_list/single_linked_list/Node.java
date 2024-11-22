baaf4648169d8e0ed4a594bdf93b42e63702cead
Dayasagar543 <dayasagar.543@gmail.com>
2024-11-22 10:36:37 +0530

    added the stack folder and moved single linked list folder into the linked list folder

---
+++ Data_structures/linked_list/single_linked_list/Node.java
@@ -0,0 +1,21 @@
+public class Node {
+    private int data;
+    private Node next = null;
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+}
